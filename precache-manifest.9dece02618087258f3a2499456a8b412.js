0484da0277435d258e999d9a8edc0a6c70a09595
Senthil <senthil@vivantatechnologies.com>
2019-04-12 15:58:45 +0530

    Updates

---
+++ precache-manifest.9dece02618087258f3a2499456a8b412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "da85f54bf94a347688da",
+    "url": "/static/js/main.2da7dbf3.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "da85f54bf94a347688da",
+    "url": "/static/css/main.43fd19e4.chunk.css"
+  },
+  {
+    "revision": "162e568bf927ba92693c30d7820eee15",
+    "url": "/index.html"
+  }
+];
